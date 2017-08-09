7273c7f4f0d8b9be1f69e4ce4bc0fcc4b64dff87
Stigjb <stigjb@gmail.com>
2017-08-09 15:03:05 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='norwayholidays',
+    version='0.1',
+    py_modules=['norwayholidays',],
+    license='MIT',
+    long_description='A calendar of holidays in Norway for use with pandas'
+    )
